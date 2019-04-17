907fea40146d7bd78f980f0eff63e110570ab82f
ironcadiz <afcadiz@uc.cl>
2019-04-16 22:22:58 -0400

    refactored rsscnn training script

--- rsscnn.py
+++ rsscnn.py
@@ -1,6 +1,11 @@
 import torchvision.models as models
 import torch.nn as nn
 
+from ignite.engine import Engine, Events
+from ignite.metrics import Accuracy,Loss, RunningAverage
+from ignite.contrib.handlers import ProgressBar
+from ignite.handlers import ModelCheckpoint
+
 class RSsCnn(nn.Module):
     
     def __init__(self):
@@ -36,77 +41,58 @@ class RSsCnn(nn.Module):
         x_rank_right = self.rank_fc_2(x_rank_right)
         return x_clf,x_rank_left, x_rank_right
 
-def update(engine, data):
-    input_left, input_right, label = data['left_image'], data['right_image'], data['winner']
-    input_left, input_right, label = input_left.to(device), input_right.to(device), label.to(device)
-    rank_label = label.clone()
-    label[label==-1] = 0
-    # zero the parameter gradients
-    optimizer.zero_grad()
-    rank_label = rank_label.float()
-    # forward + backward + optimize
-    output_clf,output_rank_left, output_rank_right = net(input_left,input_right)
 
-    loss_clf = clf_crit(output_clf,label)
-#   print(output_rank_left, output_rank_right, rank_label)
-    loss_rank = rank_crit(output_rank_left, output_rank_right, rank_label)
-    loss = loss_clf + loss_rank*lamb
-    loss.to(device)
-    loss.backward()
-    optimizer.step()
-    return  { 'loss':loss.item(), 
-            'loss_clf':loss_clf.item(), 
-            'loss_rank':loss_rank.item(),
-            'y':label,
-            'y_pred': output_clf
-            }
 
-def inference(engine,data):
-    with torch.no_grad():
+def train(device, net, dataloader, val_loader, args):
+    def update(engine, data):
         input_left, input_right, label = data['left_image'], data['right_image'], data['winner']
         input_left, input_right, label = input_left.to(device), input_right.to(device), label.to(device)
         rank_label = label.clone()
         label[label==-1] = 0
+        # zero the parameter gradients
+        optimizer.zero_grad()
         rank_label = rank_label.float()
-        # forward
+        # forward + backward + optimize
         output_clf,output_rank_left, output_rank_right = net(input_left,input_right)
+
         loss_clf = clf_crit(output_clf,label)
+    #   print(output_rank_left, output_rank_right, rank_label)
         loss_rank = rank_crit(output_rank_left, output_rank_right, rank_label)
         loss = loss_clf + loss_rank*lamb
         loss.to(device)
+        loss.backward()
+        optimizer.step()
         return  { 'loss':loss.item(), 
-            'loss_clf':loss_clf.item(), 
-            'loss_rank':loss_rank.item(),
-            'y':label,
-            'y_pred': output_clf
-            }
+                'loss_clf':loss_clf.item(), 
+                'loss_rank':loss_rank.item(),
+                'y':label,
+                'y_pred': output_clf
+                }
 
-def train():
-    net = RSsCnn()
+    def inference(engine,data):
+        with torch.no_grad():
+            input_left, input_right, label = data['left_image'], data['right_image'], data['winner']
+            input_left, input_right, label = input_left.to(device), input_right.to(device), label.to(device)
+            rank_label = label.clone()
+            label[label==-1] = 0
+            rank_label = rank_label.float()
+            # forward
+            output_clf,output_rank_left, output_rank_right = net(input_left,input_right)
+            loss_clf = clf_crit(output_clf,label)
+            loss_rank = rank_crit(output_rank_left, output_rank_right, rank_label)
+            loss = loss_clf + loss_rank*lamb
+            loss.to(device)
+            return  { 'loss':loss.item(), 
+                'loss_clf':loss_clf.item(), 
+                'loss_rank':loss_rank.item(),
+                'y':label,
+                'y_pred': output_clf
+                }
     net = net.to(device)
-
-    #torch ignite resume training
-    #MODEL_PATH='models/test_model_state_dict_4.pth'
-    #OPTIMIZER_PATH='models/test_optimizer_state_dict_4.pth'
-
-    #net = RSsCnn()
-    #optimizer = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9)
-    #net.load_state_dict(torch.load(MODEL_PATH))
-    #optimizer.load_state_dict(torch.load(OPTIMIZER_PATH))
-    #epoch = 1
-
-    # net.train()
-    # net = net.to(device)
-
-    # training with torch ignite
-    from ignite.engine import Engine, Events, create_supervised_evaluator
-    from ignite.metrics import Accuracy,Loss, RunningAverage
-    from ignite.contrib.handlers import ProgressBar
-    from ignite.handlers import ModelCheckpoint
 
     clf_crit = nn.NLLLoss()
     rank_crit = nn.MarginRankingLoss(reduction='sum')
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.wd)
     lamb = 0.5
 
 
@@ -129,21 +115,21 @@ def train():
         evaluator.run(val_loader)
         metrics = evaluator.state.metrics
         print("Training Results - Epoch: {}  Avg Val accuracy: {:.2f} Avg Val loss: {:.2f}".format(trainer.state.epoch, metrics['avg_acc'], metrics['loss']))
-        
-    # @trainer.on(Events.EPOCH_COMPLETED)
-    # def log_training_results(trainer):
-    #     evaluator.run(dataloader)
-    #     metrics = evaluator.state.metrics
-    #     print("Training Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}".format(trainer.state.epoch, metrics['avg_acc'], metrics['loss']))
+    
 
-
-    handler = ModelCheckpoint('models', 'test', save_interval=1, n_saved=2, create_dir=True, save_as_state_dict=True, require_empty=False)
+    handler = ModelCheckpoint(f'{args.model_dir}', f'{args.model}_{args.attribute}', save_interval=1, n_saved=10, create_dir=True, save_as_state_dict=True, require_empty=False)
     trainer.add_event_handler(Events.EPOCH_COMPLETED, handler, {
                 'model': net,
                 'optimizer': optimizer,
                 })
-        
-    trainer.run(dataloader,max_epochs=10)
+
+    if (args.resume):
+        def start_epoch(engine):
+            engine.state.epoch = args.epoch
+        trainer.add_event_handler(Events.STARTED, start_epoch)
+        evaluator.add_event_handler(Events.STARTED, start_epoch)
+
+    trainer.run(dataloader,max_epochs=args.max_epochs)
 
 def test():
     pass
