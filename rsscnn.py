a58a10d9b142fcc4497d972f2a1ba3f32c6ac95a
ironcadiz <afcadiz@uc.cl>
2019-09-05 16:31:49 -0400

    merge

--- rsscnn.py
+++ rsscnn.py
@@ -92,7 +92,6 @@ def train(device, net, dataloader, val_loader, args):
 
         loss_rank = rank_crit(output_rank_left, output_rank_right, rank_label)
         loss = loss_clf + loss_rank        
-
         loss.backward()
         optimizer.step()
         return  { 'loss':loss.item(), 
--- slurm_train.sh
+++ slurm_train.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=sscnn       # Nombre del trabajo
-#SBATCH --output=output/vgg_wealthy_%j.log         # Nombre del output (%j se reemplaza por el ID del trabajo)
-#SBATCH --error=output/err/vgg_wealthy_%j.err          # Output de errores (opcional)
+#SBATCH --job-name=rsscnn       # Nombre del trabajo
+#SBATCH --output=output/vgg_safety_%j.log         # Nombre del output (%j se reemplaza por el ID del trabajo)
+#SBATCH --error=output/err/vgg_safety_%j.err          # Output de errores (opcional)
 #SBATCH --ntasks=1                   # Correr 2 tareas
 #SBATCH --cpus-per-task=4            # Numero de cores por tarea
 #SBATCH --distribution=cyclic:cyclic # Distribuir las tareas de modo ciclico
-#SBATCH --time=0-02:00:00            # Timpo limite d-hrs:min:sec
-#SBATCH --mem-per-cpu=5000mb         # Memoria por proceso
+#SBATCH --time=4-00:00:00            # Timpo limite d-hrs:min:sec
+#SBATCH --mem-per-cpu=6000mb         # Memoria por proceso
 #SBATCH --mail-type=END,FAIL         # Enviar eventos al mail (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=afcadiz@uc.cl    # El mail del usuario
 #SBATCH --partition=ialab-high        # Se tiene que elegir una partición de nodos con GPU
 #SBATCH --gres=gpu:1                 # Usar 2 GPUs (se pueden usar N GPUs de marca especifica de la manera --gres=gpu:marca:N)
+#SBATCH --nodelist=hydra
 
-pyenv/bin/python3 train.py
+
+pyenv/bin/python3 train.py  --model rscnn --max_epochs 20 --premodel vgg --attribute safety --wd 0.0001 --lr 0.001  --batch_size 8 --dataset /vault/ironcadiz/placepulse --csv /vault/ironcadiz/votes_clean.csv
+
+
