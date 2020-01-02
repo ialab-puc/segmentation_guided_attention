3aeb257d608aaf06d451661e9c7f1ea7e5f60b6a
ironcadiz <afcadiz@uc.cl>
2020-01-02 17:55:25 -0300

    removed extra constants

--- nets/SegRank.py
+++ nets/SegRank.py
@@ -16,8 +16,6 @@ from segmentation.networks.pspnet import Res_Deeplab
 
 # constants
 IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
-DATA_DIRECTORY = 'pp_cropped'
-DATA_LIST_PATH = './dataset/list/placepulse/test.lst'
 IGNORE_LABEL = 255
 NUM_CLASSES = 19
 INPUT_SIZE = '340,480'
