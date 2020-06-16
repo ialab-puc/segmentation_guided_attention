e12a6ff8857ece9068078832c6ef74c0cc10f989
ironcadiz <afcadiz@uc.cl>
2020-06-16 17:00:15 -0400

    Add log sum exp loss

--- loss.py
+++ loss.py
@@ -16,13 +16,22 @@ class RankingLoss(nn.Module):
         rank_loss = torch.clamp(-y*(diff) + bin_y*self.margin, min=0)
         return torch.mean(rank_loss + tie_loss)
 
+
+
+class LogSumExpLoss(nn.Module):
+
+    def forward(self,x_left,x_right,y):
+        diff = x_left - x_right
+        bin_y = torch.abs(y)
+        tie_y =  1 - bin_y
+        rank_loss = torch.log(1 + torch.exp(-y*diff)) * bin_y
+        tie_loss = torch.log(torch.exp(diff) +  torch.exp(-diff)) * tie_y
+        print(rank_loss, tie_loss)
+        return torch.mean(rank_loss + tie_loss)
+
 if __name__ == '__main__':
-    y = torch.Tensor([-1,0,1])
-    x_left = torch.Tensor([-0.000000001,-3,-0.000000001])
-    x_right = torch.Tensor([-0.000000002,-5,-0.000000002])
-    x_left.requires_grad_()
-    x_right.requires_grad_()
-    loss = RankingLoss(margin=5)
-    result= loss(x_left,x_right,y)
-    print(result)
-    result.backward()
+    ls = LogSumExpLoss()
+    x_left = torch.Tensor([9])
+    x_right = torch.Tensor([13])
+    y = torch.Tensor([0])
+    print(ls(x_left,x_right,y))
