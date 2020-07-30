d1bb9ca3cbf4cbc89d3646b1ba1ba5a7bc4f5c5d
ironcadiz <afcadiz@uc.cl>
2020-07-30 11:55:13 -0400

    Update attention logging on training

--- utils/image_gen.py
+++ utils/image_gen.py
@@ -3,32 +3,43 @@ import numpy as np
 from torch import nn
 import cv2
 
-def segmentation_to_image(segmentation, palette, output_size=(244, 244)):
+def segmentation_to_image(segmentation,image,palette, output_size=(244, 244)):
+    grayed = gray_image(image,output_size)
     interp = nn.Upsample(size=output_size, mode='bilinear', align_corners=True)
     segmentation = interp(segmentation.unsqueeze(0)).cpu().numpy().transpose(0,2,3,1)
     seg_pred = np.asarray(np.argmax(segmentation, axis=3), dtype=np.uint8)
     output_im = PILImage.fromarray(seg_pred[0])
     output_im.putpalette(palette)
-    return output_im
+    output_im = output_im.convert('RGB')
+    result = cv2.addWeighted(np.array(output_im), 0.7, grayed, 0.3, 0)
+    return result
 
 def attention_to_images(image,attention_map,output_size=(244,244), normalize='local'):
+    interp = nn.Upsample(size=output_size, mode='bilinear', align_corners=True)
+    cvImage = gray_image(image,output_size)
+    attention_map = interp(attention_map).squeeze(1).cpu().detach().numpy()
+    ticks=[(attention_map.min()),(attention_map.max())]
+    normalized = global_normalize(cvImage, attention_map) if normalize == 'global' else local_normalize(cvImage, attention_map) 
+    return normalized, ticks
+
+def shape_attention(attention_map):
+    attention_map = attention_map.mean(dim=1, keepdim=True).permute([0,2,1])
+    attention_size = attention_map.size()
+    dim = int(attention_size[1]**(0.5))
+    attention_map = attention_map.permute([0,2,1]).view((attention_size[0],1,dim,dim))
+    return attention_map
+
+def gray_image(image,output_size):
     cvImage = cv2.cvtColor(image.cpu().numpy(), cv2.COLOR_RGB2BGR)
     cvImage = cv2.cvtColor(cvImage, cv2.COLOR_BGR2GRAY)
     cvImage = cv2.cvtColor(cvImage, cv2.COLOR_GRAY2BGR) #we neeed a 3 dimensional gray image
     cvImage = cv2.resize(cvImage, output_size)
-    attention_map = attention_map[:,:,0:960]
-    interp = nn.Upsample(size=output_size, mode='bilinear', align_corners=True)
-    attention_map = attention_map.norm(dim=2, keepdim=True)
-    attention_size = attention_map.size()
-    dim = int(attention_size[1]**(0.5))
-    attention_map = attention_map.view((attention_size[0],1,dim,dim))
-    attention_map = interp(attention_map).squeeze(1).cpu().detach().numpy()
-    return global_normalize(cvImage, attention_map) if normalize == 'global' else local_normalize(cvImage, attention_map)
+    return cvImage
 
-def global_normalize(image,attention_map):
+def global_normalize(image,attention_map, mask_value=-1):
     images = []
     heatmap_img = None
-    heatmap_img = cv2.normalize(attention_map, heatmap_img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    heatmap_img = normalize_attention(attention_map, mask_value)
     for single_map in heatmap_img:
         single_img = cv2.applyColorMap(single_map, cv2.COLORMAP_JET)
         result = cv2.addWeighted(single_img, 0.5, image, 0.5, 0)
@@ -36,16 +47,21 @@ def global_normalize(image,attention_map):
         images.append(result)
     return images
 
-def local_normalize(image,attention_map):
+def local_normalize(image,attention_map, mask_value=-1):
     images = []
     for single_map in attention_map:
         heatmap_img = None
-        heatmap_img = cv2.normalize(single_map, heatmap_img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        heatmap_img = normalize_attention(single_map, mask_value)
         img = cv2.applyColorMap(heatmap_img, cv2.COLORMAP_JET)
         result = cv2.addWeighted(img, 0.5, image, 0.5, 0)
         result = cv2.cvtColor(result,cv2.COLOR_BGR2RGB)
         images.append(result)
     return images
+
+def normalize_attention(attention, mask_value=-1):
+    input_min = np.min(attention[attention > mask_value])
+    input_max = attention.max()
+    return (np.maximum((attention - input_min),0)/(input_max - input_min) * 255).astype('uint8')
 
 def get_palette(num_cls):
     """ Returns the color map for visualizing the segmentation mask.
--- utils/log.py
+++ utils/log.py
@@ -1,5 +1,5 @@
 import json
-from utils.image_gen import segmentation_to_image, attention_to_images
+from utils.image_gen import segmentation_to_image, attention_to_images, shape_attention
 
 def tb_log(train_metrics,val_metrics,writer,attribute, epoch):
     for key,metric_hash in train_metrics.items():
@@ -20,13 +20,12 @@ def comet_log(metrics, experiment, epoch=None, step=None):
 def comet_image_log(image,image_name,experiment,epoch=None):
     experiment.log_image(image, name=image_name)
 
-
 def image_log(segmentation,original,attention_map,palette,experiment,epoch, normalize='local'):
     if segmentation is not None:
-        seg_img = segmentation_to_image(segmentation,palette)
+        seg_img = segmentation_to_image(segmentation, original, palette)
         comet_image_log(seg_img,f'segmentation_epoch:{epoch}',experiment, epoch=epoch)
     if attention_map is not None:
-        attentions = attention_to_images(original, attention_map, normalize=normalize)
+        attentions, _ = attention_to_images(original, shape_attention(attention_map), normalize=normalize)
         for i,image in enumerate(attentions):
             comet_image_log(image,f'attention_head:{i}_epoch:{epoch}',experiment, epoch=epoch)
     comet_image_log(original,f'original_epoch{epoch}',experiment, epoch=epoch)
